4d596accc3c55ced75dca8fd3838792745039709
Nikoly <turnik3580@gmail.com>
2020-08-23 12:22:43 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double weight = 70;
+        double hight = 1.85;
+
+        double bodyMassIndex;
+
+        bodyMassIndex = weight / (hight * hight);
+
+        System.out.println(bodyMassIndex);
+
+    }
+}
+
